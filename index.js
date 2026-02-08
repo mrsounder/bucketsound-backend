d342032669ea85cb4ed20a9a6de9c44eea257667
mrsounder <mrsounder00@gmail.com>
2026-02-08 17:40:31 +0900

    clean code without comments

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//
-const dbURI = 'mongodb+srv://mrsounder00:pxDbc4Au0bFgwL0g@cluster0.abcde.mongodb.net/bucketsound?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://mrsounder00:pxDbc4Au0bFgwL0g@cluster0.v4gxjro.mongodb.net/bucketsound?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI)
-  .then(() => console.log('DB Connected! :tada:'))
+  .then(() => console.log('DB Connected! 🎉'))
   .catch((err) => console.log('DB Error:', err));
 
 app.use(express.json());
@@ -20,6 +19,22 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+const User = mongoose.model('User', new mongoose.Schema({
+  userId: String,
+  userPw: String
+}));
+
+app.post('/api/register', async (req, res) => {
+  try {
+    const { userId, userPw } = req.body;
+    const newUser = new User({ userId, userPw });
+    await newUser.save();
+    res.json({ success: true });
+  } catch (err) {
+    res.json({ success: false });
+  }
 });
 
 app.listen(port, () => {
